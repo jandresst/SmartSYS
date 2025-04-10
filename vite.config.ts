7a4ea3edeafc49fa6e8528c2e6a1a65928d6e4e0
Jean <jeandsgperez@gmail.com>
2025-04-10 10:00:59 -0400

    Pages Added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin' // Nota el cambio de nombre
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 3 },
+      mozjpeg: { quality: 80 },
+      optipng: { optimizationLevel: 5 },
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox' },
+          { name: 'removeEmptyAttrs', active: false }
+        ]
+      }
+    })
+  ]
 })
